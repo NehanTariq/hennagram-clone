bb13ab0d6ffe89f9be3a21362d56c2d4a01570a6
NehanTariq <nehan.shujia@gmail.com>
2023-06-07 11:14:51 +0400

    removed

--- resources/js/Components/Icons/Toggle.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<svg width="20" height="20" viewBox="0 0 26 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="3.3623" cy="3.82545" r="3" fill="#18181A"/>
-<circle cx="13.0229" cy="3.82545" r="3" fill="#18181A"/>
-<circle cx="22.6838" cy="3.82545" r="3" fill="#18181A"/>
-</svg>
-</template>
