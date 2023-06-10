513ba6be2c50be3549091ca815339c20dc2867f2
NehanTariq <nehan.shujia@gmail.com>
2023-06-10 17:10:36 +0400

    icons resizing

--- resources/js/Components/LikesSection.vue
+++ resources/js/Components/LikesSection.vue
@@ -2,11 +2,17 @@
 import {computed , toRefs} from 'vue';
 import {usePage} from '@inertiajs/vue3';
 
-import Heart from 'vue-material-design-icons/Heart.vue';
-import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
-import CommentOutline from 'vue-material-design-icons/CommentOutline.vue';
-import SendOutline from 'vue-material-design-icons/SendOutline.vue';
-import BookmarkOutline from 'vue-material-design-icons/BookmarkOutline.vue';
+import LikedHeart from './Icons/LikedHeart.vue';
+import Like from './Icons/Like.vue';
+import Comment from './Icons/Comment.vue';
+import Share from './Icons/Share.vue';
+import Save from './Icons/Save.vue';
+
+// import Heart from 'vue-material-design-icons/Heart.vue';
+// import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+// import CommentOutline from 'vue-material-design-icons/CommentOutline.vue';
+// import SendOutline from 'vue-material-design-icons/SendOutline.vue';
+// import BookmarkOutline from 'vue-material-design-icons/BookmarkOutline.vue';
 
 const props = defineProps(['post'])
 const { post } = toRefs(props)
@@ -30,13 +36,13 @@ const isHeartActiveComputed = computed(()=>{
 <template>
     <div class="flex z-20 items-center justify-between">
         <div class="flex items-center">
-            <button @click="$emit('like', {post, user})" class="-mt-[14px]">
-                <HeartOutline v-if="!isHeartActiveComputed" class="pl-3 cursor-pointer" :size="30" />
-                <Heart v-else class="pl-3 cursor-pointer" fillColor="#FF0000" :size="30"/>
+            <button @click="$emit('like', {post, user})" class="-mt-[11px]">
+                <Like v-if="!isHeartActiveComputed" class="cursor-pointer" />
+                <LikedHeart v-else class="cursor-pointer" />
             </button>
-            <CommentOutline class="pl-3 pt-[10px] cursor-pointer" :size="30" />
-            <SendOutline class="pl-3 pt-[10px] cursor-pointer" :size="30" />
+            <Comment class="pl-3 pt-[10px] cursor-pointer" />
+            <Share class="pl-3 pt-[10px] cursor-pointer" />
         </div>
-        <BookmarkOutline class="pl-3 pt-[10px] cursor-pointer" :size="30" />
+        <Save class="pl-3 pt-[10px] cursor-pointer" />
     </div>
 </template>
